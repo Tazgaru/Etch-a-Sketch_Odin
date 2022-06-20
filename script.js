44f7eaf003e1444ece7c830330147aec4c42eda4
Tazgaru <mauriciokrusser@gmail.com>
2022-06-20 16:21:08 -0500

    Add createGrid and changeColor functions

---
+++ script.js
@@ -0,0 +1,39 @@
+const canvasDiv = document.querySelector('.canvas');
+const newGridBtn = document.querySelector('.new-grid-btn');
+const clearGridBtn = document.querySelector('.clear-grid-btn');
+
+newGridBtn.addEventListener('click', createGrid);
+
+
+function createGrid(){
+
+    removeAllChildNodes(canvasDiv);
+
+    let numOfPixels = prompt('New Grid Size');
+    numOfPixels = +numOfPixels;
+    canvasDiv.setAttribute('style', `grid-template-columns: repeat(${numOfPixels}, auto)`);
+
+
+    for (let i = 0; i < numOfPixels; i++){
+        for (let j = 0; j < numOfPixels; j++) {
+
+            let pixelDiv = document.createElement('div');
+            pixelDiv.classList.add('pixel');
+            canvasDiv.appendChild(pixelDiv);
+            pixelDiv.addEventListener('mouseenter', changeColor, {once:true});
+        }
+    }
+}
+
+function changeColor() {
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    this.setAttribute('style', `background-color: rgb(${r} ${g} ${b})`);
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
